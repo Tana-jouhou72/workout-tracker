574381fc66b4d84cce7b210d4affb48f54e1fc2c
Tana-jouhou72 <121955190+Tana-jouhou72@users.noreply.github.com>
2026-05-20 14:08:36 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,16 @@
+self.addEventListener('install', (event) => {
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(self.clients.claim());
+});
+
 self.addEventListener('fetch', (event) => {
-    // Required to satisfy Android's installation criteria
+    // True network proxy to guarantee Android Chrome validates the Service Worker criteria
+    event.respondWith(
+        fetch(event.request).catch(() => {
+            return new Response("Offline proxy active.");
+        })
+    );
 });
